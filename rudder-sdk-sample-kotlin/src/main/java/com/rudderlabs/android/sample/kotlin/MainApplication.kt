56b795f553198b2db0147f6530c6f0891e20e97b
arnab <arnab@rudderlabs.com>
2019-10-09 14:46:47 +0530

    fixed crash for empty factories

--- rudder-sdk-sample-kotlin/src/main/java/com/rudderlabs/android/sample/kotlin/MainApplication.kt
+++ rudder-sdk-sample-kotlin/src/main/java/com/rudderlabs/android/sample/kotlin/MainApplication.kt
@@ -8,7 +8,7 @@ class MainApplication : Application() {
     companion object {
 //        private const val WRITE_KEY = "1R3JbxsqWZlbYjJlBxf0ZNWZOH6"
         private const val WRITE_KEY = "1R3JbxsqWZlbYjJlBxf0ZNWZOH6"
-        private const val END_POINT_URI = "https://5c40a958.ngrok.io"
+        private const val END_POINT_URI = "https://04df83ab.ngrok.io"
         lateinit var rudderClient: RudderClient
     }
 
