282f1f5ac298044f75be17c1ed0e106c4dcfb22f
arnab <arnab@rudderlabs.com>
2020-01-07 18:14:13 +0530

    added RudderConfig as a part of native SDK integration

--- rudder-sdk-core/src/main/java/com/rudderlabs/android/sdk/core/RudderIntegration.java
+++ rudder-sdk-core/src/main/java/com/rudderlabs/android/sdk/core/RudderIntegration.java
@@ -2,35 +2,14 @@ package com.rudderlabs.android.sdk.core;
 
 public abstract class RudderIntegration<T> {
     public interface Factory {
-        RudderIntegration<?> create(Object settings, RudderClient client);
+        RudderIntegration<?> create(Object settings, RudderClient client, RudderConfig config);
 
         String key();
     }
 
-    public void identify(RudderMessage identify) {
-    }
+    public abstract void reset();
 
-    public void group(RudderMessage group) {
-    }
-
-    public void track(RudderMessage track) {
-    }
-
-    public void alias(RudderMessage alias) {
-    }
-
-    public void screen(RudderMessage screen) {
-    }
-
-    public void flush() {
-    }
-
-    public void reset() {
-    }
-
-    public void dump(RudderMessage element) {
-
-    }
+    public abstract void dump(RudderMessage element);
 
     /**
      * @return Instance of the initiated SDK
