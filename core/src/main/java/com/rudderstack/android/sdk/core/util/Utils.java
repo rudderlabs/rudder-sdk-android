06a42d32da7c2dedea458268ce081acef655bbab
arnab <arnab@rudderlabs.com>
2020-04-07 09:40:32 +0530

    Updated size limit constants

--- core/src/main/java/com/rudderstack/android/sdk/core/util/Utils.java
+++ core/src/main/java/com/rudderstack/android/sdk/core/util/Utils.java
@@ -30,8 +30,8 @@ public class Utils {
     public static final int MIN_SLEEP_TIMEOUT = 10;
     public static final int MIN_FLUSH_QUEUE_SIZE = 1;
     public static final int MAX_FLUSH_QUEUE_SIZE = 100;
-    public static final int MAX_EVENT_SIZE = 2 * 1024;
-    public static final int MAX_BATCH_SIZE = 5 * 1024;
+    public static final int MAX_EVENT_SIZE = 32 * 1024; // 32 KB
+    public static final int MAX_BATCH_SIZE = 500 * 1024; // 500 KB
 
     public static String getTimeZone() {
         TimeZone timeZone = TimeZone.getDefault();
