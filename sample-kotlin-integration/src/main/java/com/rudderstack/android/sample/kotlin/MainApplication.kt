ff6c0a2510467440fa2ab61694cc32a719e8da4a
arnab <arnab@rudderlabs.com>
2020-05-27 23:52:39 +0530

    Updated sample application

--- sample-kotlin-integration/src/main/java/com/rudderstack/android/sample/kotlin/MainApplication.kt
+++ sample-kotlin-integration/src/main/java/com/rudderstack/android/sample/kotlin/MainApplication.kt
@@ -12,7 +12,7 @@ class MainApplication : Application() {
     companion object {
         var rudderClient: RudderClient? = null
         const val TAG = "MainApplication"
-        const val END_URL = "https://cd740f82.ngrok.io"
+        const val DATA_PLANE_URL = "https://cd740f82.ngrok.io"
         const val WRITE_KEY = "1TSRSskqa15PG7F89tkwEbl5Td8"
     }
 
@@ -23,59 +23,11 @@ class MainApplication : Application() {
             this,
             WRITE_KEY,
             RudderConfig.Builder()
-                .withDataPlaneUrl(END_URL)
-                .withLogLevel(RudderLogger.RudderLogLevel.VERBOSE)
-                .withControlPlaneUrl(END_URL)
+                .withDataPlaneUrl(DATA_PLANE_URL)
+                .withLogLevel(RudderLogger.RudderLogLevel.DEBUG)
                 .withTrackLifecycleEvents(true)
                 .withRecordScreenViews(true)
                 .build()
         )
-        rudderClient!!.rudderContext.putDeviceToken("Some_device_token")
-
-        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
-            override fun onActivityPaused(p0: Activity) {
-                Log.d(TAG, "onActivityPaused ${p0.localClassName}")
-            }
-
-            override fun onActivityStarted(p0: Activity) {
-                Log.d(TAG, "onActivityStarted ${p0.localClassName}")
-            }
-
-            override fun onActivityDestroyed(p0: Activity) {
-                Log.d(TAG, "onActivityDestroyed ${p0.localClassName}")
-            }
-
-            override fun onActivitySaveInstanceState(p0: Activity, p1: Bundle) {
-                Log.d(TAG, "onActivitySaveInstanceState ${p0.localClassName}, Bundle: $p1")
-            }
-
-            override fun onActivityStopped(p0: Activity) {
-                Log.d(TAG, "onActivityStopped ${p0.localClassName}")
-            }
-
-            override fun onActivityCreated(p0: Activity, p1: Bundle?) {
-                Log.d(TAG, "onActivityCreated ${p0.localClassName} Bundle: ${p1.toString()}")
-            }
-
-            override fun onActivityResumed(p0: Activity) {
-                Log.d(TAG, "onActivityResumed ${p0.localClassName}")
-            }
-
-        })
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        Log.d(TAG, "onLowMemory")
-    }
-
-    override fun onTerminate() {
-        super.onTerminate()
-        Log.d(TAG, "onTerminate")
-    }
-
-    override fun onTrimMemory(level: Int) {
-        super.onTrimMemory(level)
-        Log.d(TAG, "onTrimMemory: $level")
     }
 }
