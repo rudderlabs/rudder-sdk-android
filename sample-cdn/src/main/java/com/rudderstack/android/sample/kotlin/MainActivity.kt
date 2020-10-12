c18ad1989702df516ba92efe35df065cd87ccf0c
Arnab <arnab.compsc@gmail.com>
2020-10-13 01:43:41 +0530

    Added sample application for checking SDK from CDN

---
+++ sample-cdn/src/main/java/com/rudderstack/android/sample/kotlin/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.rudderstack.android.sample.kotlin
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.adjust.sdk.Adjust
+import com.rudderstack.android.sdk.core.RudderOption
+import com.rudderstack.android.sdk.core.RudderTraits
+
+
+class MainActivity : AppCompatActivity() {
+    private var count = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        Handler().postDelayed({
+//            MainApplication.rudderClient!!.identify(
+//                "test_user_id",
+//                RudderTraits()
+//                    .putFirstName("Test First Name")
+//                    .putEmail("random2@gmail.com"),
+//                RudderOption()
+//                    .putExternalId("brazeExternalId", "434dfbfb-c5bf-46d0-a47e-1336fcea264c")
+//            )
+//
+//            MainApplication.rudderClient!!.track("Test Event")
+//            MainApplication.rudderClient!!.track("Test Event 1")
+//            MainApplication.rudderClient!!.track("Test Event 2")
+//            MainApplication.rudderClient!!.track("Test Event 3")
+//            MainApplication.rudderClient!!.track("Test Event 4")
+//            MainApplication.rudderClient!!.track("Test Event 5")
+//
+//        }, 2000)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Adjust.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Adjust.onPause()
+    }
+}
