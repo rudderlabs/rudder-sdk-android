89ef619d4aa1cb3cf267c23400f7116d4413e09b
dhawal1248 <sanghvi.dhawal101433@gmail.com>
2020-11-06 20:02:09 +0530

    Add initial unit tests

--- core/src/test/java/com/rudderstack/android/sdk/core/RudderClientTest.java
+++ core/src/test/java/com/rudderstack/android/sdk/core/RudderClientTest.java
@@ -1,10 +1,16 @@
 package com.rudderstack.android.sdk.core;
 
+import androidx.test.core.app.ApplicationProvider;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -13,25 +19,78 @@ import static org.junit.Assert.assertNotNull;
 public class RudderClientTest {
 
     private RudderClient client;
+    private String writeKey = "1234";
+    private String dataplaneUrl = "https//hosted.rudderlabs.com";
+    private final ArrayList<RudderMessage> messageStore = new ArrayList<>();
 
     @Before
     public void setUp() {
-        System.out.println("setup called");
+        this.client = RudderClient.getInstance(
+                ApplicationProvider.getApplicationContext(),
+                writeKey,
+                new RudderConfig.Builder()
+                        .withDataPlaneUrl(dataplaneUrl)
+                        .withLogLevel(RudderLogger.RudderLogLevel.DEBUG)
+                        .build()
+        );
+        try {
+            // get the repository variable from client
+            Field repository = client.getClass().getDeclaredField("repository");
+            // make private variable repository accessible
+            repository.setAccessible(true);
+            // create EventRepository mock
+            EventRepository repositoryMock = Mockito.mock(EventRepository.class);
+            // mock EventRepository.dump to store dumped message to messageStore
+            Mockito.doAnswer(i -> {
+                System.out.println("Dumped :"+i.getArguments());
+                messageStore.add(0, (RudderMessage) i.getArguments()[0]);
+                return null;
+            }).when(repositoryMock).dump(Mockito.any(RudderMessage.class));
+            // replace the original repository with the mocked one
+            repository.set(null, repositoryMock);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
 
+    RudderMessage getLatestDump() {
+        RudderMessage message = null;
+        if (messageStore.size() > 0) {
+            message = messageStore.get(0);
+            // remove the message to keep only one message in the store at a time
+            messageStore.remove(0);
+        }
+        return message;
     }
 
     @Test
-    public void shouldNotBeNull() {
-        System.out.println(this.client);
-        assertNotNull(this.client);
+    public void getInstance_NoRudderConfig() {
+        System.out.println(String.format("Calling getInstance with config - writeKey %s", 1234));
+        RudderClient testClient = RudderClient.getInstance(
+                ApplicationProvider.getApplicationContext(),
+                writeKey
+        );
+        assertNotNull(testClient);
+        System.out.println(testClient);
+        System.out.println("Rudder Client successfully initialized");
     }
 
     @Test
-    public void getInstance() {
-    }
-
-    @Test
-    public void getInstance1() {
+    public void getInstance_WithRudderConfigNoFactories() {
+        System.out.println(String.format("Calling getInstance with config - writeKey %s", 1234));
+        RudderClient testClient = RudderClient.getInstance(
+                ApplicationProvider.getApplicationContext(),
+                writeKey,
+                new RudderConfig.Builder()
+                .withDataPlaneUrl(dataplaneUrl)
+                .withLogLevel(RudderLogger.RudderLogLevel.DEBUG)
+                .build()
+        );
+        assertNotNull(testClient);
+        System.out.println(testClient);
+        System.out.println("Rudder Client successfully initialized");
     }
 
     @Test
@@ -51,23 +110,66 @@ public class RudderClientTest {
     }
 
     @Test
-    public void track() {
+    public void track_OnlyEventName() {
+        client.track("testEvent");
+        RudderMessage message = getLatestDump();
+        assert(message.getEventName() == "testEvent");
+        System.out.println("Test track_OnlyEventName passed");
     }
 
     @Test
-    public void track1() {
+    public void track_EventNameAndProperties() {
+        RudderProperty properties = new RudderProperty().putValue("key", "value");
+        client.track("testEvent", properties);
+        RudderMessage message = getLatestDump();
+        assert(message.getEventName() == "testEvent");
+        assert(message.getProperties().equals(properties.getMap()));
+        System.out.println("Test track_EventNameAndProperties passed");
     }
 
     @Test
-    public void track2() {
+    public void track_EventNamePropertiesAndOptions() {
+        RudderProperty properties = new RudderProperty().putValue("key", "value");
+        RudderOption options = new RudderOption().putExternalId("braze_externalId", "1234");
+        client.track("testEvent", properties, options);
+        RudderMessage message = getLatestDump();
+        assert(message.getEventName() == "testEvent");
+        assert(message.getProperties().equals(properties.getMap()));
+        assert(message.getRudderOption().getExternalIds() == options.getExternalIds());
+        System.out.println("Test track_EventNamePropertiesAndOptions passed");
     }
 
     @Test
-    public void track3() {
+    public void track_RudderMessage() {
+        RudderProperty properties = new RudderProperty().putValue("key", "value");
+        RudderOption options = new RudderOption().putExternalId("braze_externalId", "1234");
+        RudderMessage inputMessage = new RudderMessageBuilder()
+                .setEventName("testEvent")
+                .setProperty(properties)
+                .setRudderOption(options)
+                .build();
+        client.track(inputMessage);
+        RudderMessage message = getLatestDump();
+        assert(message.getEventName() == "testEvent");
+        assert(message.getProperties().equals(properties.getMap()));
+        assert(message.getRudderOption().getExternalIds() == options.getExternalIds());
+        System.out.println("Test track_RudderMessage passed");
     }
 
     @Test
-    public void track4() {
+    public void track_RudderMessageBuilder() {
+        RudderProperty properties = new RudderProperty().putValue("key", "value");
+        RudderOption options = new RudderOption().putExternalId("braze_externalId", "1234");
+        RudderMessageBuilder builder = new RudderMessageBuilder()
+                .setEventName("testEvent")
+                .setProperty(properties)
+                .setRudderOption(options);
+        client.track(builder);
+        RudderMessage message = getLatestDump();
+        assert(message.getEventName() == "testEvent");
+        assert(message.getProperties().equals(properties.getMap()));
+        assert(message.getRudderOption().getExternalIds() == options.getExternalIds());
+        System.out.println("Test track_RudderMessageBuilder passed");
     }
 
     @Test
