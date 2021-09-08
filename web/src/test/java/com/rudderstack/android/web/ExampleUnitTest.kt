22a08e14a2b0b85f7ba3b193db5906f5e90428dd
Debanjan <debanjanchatterjee99@gmail.com>
2021-09-09 00:41:41 +0530

    develop formed

---
+++ web/src/test/java/com/rudderstack/android/web/ExampleUnitTest.kt
@@ -0,0 +1,25 @@
+package com.rudderstack.android.web
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+    @Test
+    fun testReified(){
+        val testMap = reifiedStr<Map<String,Int>>()
+        println("test: $testMap")
+    }
+    private inline  fun <reified T> reifiedStr(): String?{
+        return T::class.java.canonicalName
+    }
+}
