cee1bfcdec8cf7449a0db0e5d739e0a0bc91ea1c
1abhishekpandey <abhishekpandey10196@gmail.com>
2021-09-23 17:06:20 +0530

    Addressed comments

--- core/src/main/java/com/rudderstack/android/sdk/core/EventRepository.java
+++ core/src/main/java/com/rudderstack/android/sdk/core/EventRepository.java
@@ -78,14 +78,10 @@ class EventRepository implements Application.ActivityLifecycleCallbacks {
             // initiate RudderPreferenceManager
             this.preferenceManager = RudderPreferenceManager.getInstance(_application);
             if (preferenceManager.getOptStatus()) {
-                if (_anonymousId != null) {
-                    _anonymousId = null;
-                    RudderLogger.logDebug("User Opted out for tracking the activity, hence dropping the anonymousId");
-                }
-                if (_advertisingId != null) {
-                    _advertisingId = null;
-                    RudderLogger.logDebug("User Opted out for tracking the activity, hence dropping the advertisingId");
-                }
+                _anonymousId = null;
+                RudderLogger.logDebug("User Opted out for tracking the activity, hence dropping the anonymousId");
+                _advertisingId = null;
+                RudderLogger.logDebug("User Opted out for tracking the activity, hence dropping the advertisingId");
             }
 
             // 2. initiate RudderElementCache
