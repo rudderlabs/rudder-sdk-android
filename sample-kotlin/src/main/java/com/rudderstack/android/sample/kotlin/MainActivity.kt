1727b79fcb73fb93b2f859a26c6aab31c885c6ae
Desu Sai Venkat <saivenkatdesu@gmail.com>
2021-11-01 13:36:05 +0530

    Reverted Changes in Sample-Kotlin App

--- sample-kotlin/src/main/java/com/rudderstack/android/sample/kotlin/MainActivity.kt
+++ sample-kotlin/src/main/java/com/rudderstack/android/sample/kotlin/MainActivity.kt
@@ -17,58 +17,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val traits = RudderTraits()
-        traits.putEmail("abc@123.com")
-        traits.putFirstName("First")
-        traits.putLastName("Last")
-        traits.putGender("m")
-        traits.putPhone("5555555555")
-        MainApplication.rudderClient!!.identify("test_user_id", traits, null)
-
-        MainApplication.rudderClient!!.track("First Track")
-
-        RudderClient.setAnonymousId("AnonId1");
-
-        MainApplication.rudderClient!!.track("Anon 1 Track")
-
-        RudderClient.setAnonymousId("AnonId2");
-
-        MainApplication.rudderClient!!.track("Anon 2 Track")
-
+        val option = RudderOption()
+            .putExternalId("brazeExternalId", "some_external_id_1")
+            .putExternalId("braze_id", "some_braze_id_2")
+            .putIntegration("GA", true).putIntegration("Amplitude", true)
+            .putCustomContext(
+                "customContext", mapOf(
+                    "version" to "1.0.0",
+                    "language" to "kotlin"
+                )
+            )
+        MainApplication.rudderClient!!.identify(
+            "userId",
+            RudderTraits().putFirstName("Test First Name"),
+            option
+        )
         MainApplication.rudderClient!!.reset()
+        val props = RudderProperty()
+        props.put("Name", "John")
+        props.put("city", "NYC")
+        MainApplication.rudderClient!!.track("test event john", props, option)
 
-        MainApplication.rudderClient!!.track("After reset")
+        MainApplication.rudderClient!!.track("Test Event")
 
+        MainApplication.rudderClient!!.onIntegrationReady("App Center", NativeCallBack("App Center"));
 
-//         val option = RudderOption()
-//                    .putExternalId("brazeExternalId", "some_external_id_1")
-//                    .putExternalId("braze_id", "some_braze_id_2")
-//                    .putIntegration("GA", true).putIntegration("Amplitude", true)
-//                    .putCustomContext(
-//                        "customContext", mapOf(
-//                            "version" to "1.0.0",
-//                            "language" to "kotlin"
-//                        )
-//                    )
-//            MainApplication.rudderClient!!.identify(
-//                "userId",
-//                RudderTraits().putFirstName("Test First Name"),
-//                option
-//            )
-//            MainApplication.rudderClient!!.reset()
-//            val props = RudderProperty()
-//            props.put("Name", "John")
-//            props.put("city", "NYC")
-//            MainApplication.rudderClient!!.track("test event john", props, option)
-//
-//            MainApplication.rudderClient!!.track("Test Event")
-//
-//            MainApplication.rudderClient!!.onIntegrationReady("App Center", NativeCallBack("App Center"));
-//
-//        MainApplication.rudderClient!!.onIntegrationReady("Custom Factory", NativeCallBack("Custom Factory"));
+        MainApplication.rudderClient!!.onIntegrationReady("Custom Factory", NativeCallBack("Custom Factory"));
 
 
-       
+
     }
 }
 
