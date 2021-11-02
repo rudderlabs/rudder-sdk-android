956921e31f6f05595465b3a15b32fcd8e55b274b
Desu Sai Venkat <saivenkatdesu@gmail.com>
2021-11-02 22:40:49 +0530

    Bumped version to 1.0.22

--- sample-kotlin/src/main/java/com/rudderstack/android/sample/kotlin/MainActivity.kt
+++ sample-kotlin/src/main/java/com/rudderstack/android/sample/kotlin/MainActivity.kt
@@ -2,55 +2,151 @@ package com.rudderstack.android.sample.kotlin
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.rudderstack.android.sdk.core.RudderClient
-import com.rudderstack.android.sdk.core.RudderOption
-import com.rudderstack.android.sdk.core.RudderProperty
-import com.rudderstack.android.sdk.core.RudderTraits
-import java.lang.annotation.Native
-
+import com.rudderstack.android.sample.kotlin.MainApplication
+import com.rudderstack.android.sample.kotlin.R
 
 class MainActivity : AppCompatActivity() {
-    private var count = 0
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-         val option = RudderOption()
-                    .putExternalId("brazeExternalId", "some_external_id_1")
-                    .putExternalId("braze_id", "some_braze_id_2")
-                    .putIntegration("GA", true).putIntegration("Amplitude", true)
-                    .putCustomContext(
-                        "customContext", mapOf(
-                            "version" to "1.0.0",
-                            "language" to "kotlin"
-                        )
-                    )
-            MainApplication.rudderClient!!.identify(
-                "userId",
-                RudderTraits().putFirstName("Test First Name"),
-                option
-            )
-            MainApplication.rudderClient!!.reset()
-            val props = RudderProperty()
-            props.put("Name", "John")
-            props.put("city", "NYC")
-            MainApplication.rudderClient!!.track("test event john", props, option)
-
-            MainApplication.rudderClient!!.track("Test Event")
-
-            MainApplication.rudderClient!!.onIntegrationReady("App Center", NativeCallBack("App Center"));
-
-        MainApplication.rudderClient!!.onIntegrationReady("Custom Factory", NativeCallBack("Custom Factory"));
+//        Thread1().start()
+//        Thread2().start()
+//        Thread3().start()
+//        Thread4().start()
+//        Thread5().start()
+//        Thread6().start()
+//        Thread7().start()
+//        Thread8().start()
+//        Thread9().start()
+//        Thread10().start()
 
 
-       
+        var count = 1
+
+        while (count <= 10) {
+            println("Main Thread Event: " + count);
+            MainApplication.rudderClient!!.track("Main Thread Event : " + count)
+            count++;
+        }
     }
 }
 
-internal class NativeCallBack(private val integrationName: String) : RudderClient.Callback {
-    override fun onReady(instance: Any) {
-        println("Call back of integration : "+integrationName +" is called");
+
+class Thread1 : Thread() {
+    public override fun run() {
+        var count = 1
+        while (count <= 5000) {
+            println("Thread 1 Event: " + count);
+            MainApplication.rudderClient!!.track("Thread 1 Event : " + count)
+            count++;
+        }
+    }
+}
+
+class Thread2 : Thread() {
+    public override fun run() {
+        var count = 1
+        while (count <= 5000) {
+            println("Thread 2 Event: " + count);
+            MainApplication.rudderClient!!.track("Thread 2 Event : " + count)
+            count++;
+        }
+    }
+}
+
+class Thread3 : Thread() {
+    public override fun run() {
+        var count = 1
+        while (count <= 5000) {
+            println("Thread 3 Event: " + count);
+            MainApplication.rudderClient!!.track("Thread 3 Event : " + count)
+            count++;
+        }
+    }
+}
+
+class Thread4 : Thread() {
+    public override fun run() {
+        var count = 1
+
+        while (count <= 5000) {
+            println("Thread 4 Event: " + count);
+            MainApplication.rudderClient!!.track("Thread 4 Event : " + count)
+            count++;
+        }
+    }
+}
+
+class Thread5 : Thread() {
+    public override fun run() {
+        var count = 1
+
+        while (count <= 5000) {
+            println("Thread 5 Event: " + count);
+            MainApplication.rudderClient!!.track("Thread 5 Event : " + count)
+            count++;
+        }
+    }
+}
+
+class Thread6 : Thread() {
+    public override fun run() {
+        var count = 1
+
+        while (count <= 5000) {
+            println("Thread 6 Event: " + count);
+            MainApplication.rudderClient!!.track("Thread 6 Event : " + count)
+            count++;
+        }
+    }
+}
+
+class Thread7 : Thread() {
+    public override fun run() {
+        var count = 1
+
+        while (count <= 5000) {
+            println("Thread 7 Event: " + count);
+            MainApplication.rudderClient!!.track("Thread 7 Event : " + count)
+            count++;
+        }
+    }
+}
+
+class Thread8 : Thread() {
+    public override fun run() {
+        var count = 1
+
+        while (count <= 5000) {
+            println("Thread 8 Event: " + count);
+            MainApplication.rudderClient!!.track("Thread 8 Event : " + count)
+            count++;
+        }
+    }
+}
+
+class Thread9 : Thread() {
+    public override fun run() {
+        var count = 1
+
+        while (count <= 5000) {
+            println("Thread 9 Event: " + count);
+            MainApplication.rudderClient!!.track("Thread 9 Event : " + count)
+            count++;
+        }
+    }
+}
+
+class Thread10 : Thread() {
+    public override fun run() {
+        var count = 1
+
+        while (count <= 5000) {
+            println("Thread 10 Event: " + count);
+            MainApplication.rudderClient!!.track("Thread 10 Event : " + count)
+            count++;
+        }
     }
 }
--- sample-kotlin/src/main/java/com/rudderstack/android/sample/kotlin/MainApplication.kt
+++ sample-kotlin/src/main/java/com/rudderstack/android/sample/kotlin/MainApplication.kt
@@ -2,15 +2,17 @@ package com.rudderstack.android.sample.kotlin
 
 import android.app.Application
 import android.os.Handler
+import android.os.StrictMode
 import com.rudderstack.android.sdk.core.RudderClient
 import com.rudderstack.android.sdk.core.RudderConfig
 import com.rudderstack.android.sdk.core.RudderLogger
+import com.squareup.leakcanary.LeakCanary
 
 class MainApplication : Application() {
     companion object {
         var rudderClient: RudderClient? = null
         const val TAG = "MainApplication"
-        const val DATA_PLANE_URL = "https://e5757ebd4d1a.ngrok.io"
+        const val DATA_PLANE_URL = "https://3b09-2409-4070-4e0a-1bf5-3c78-9ef4-50c8-2ff3.ngrok.io"
         const val CONTROL_PLANE_URL = "https://0e741f50e567.ngrok.io"
         const val WRITE_KEY = "1pTxG1Tqxr7FCrqIy7j0p28AENV"
     }
@@ -18,34 +20,28 @@ class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-//        rudderClient = RudderClient.getInstance(
-//            this,
-//            WRITE_KEY,
-//            RudderConfig.Builder()
-//                .withDataPlaneUrl(DATA_PLANE_URL)
-//                .withLogLevel(RudderLogger.RudderLogLevel.DEBUG)
-//                .withTrackLifecycleEvents(false)
-//                .withRecordScreenViews(false)
-//                .build(), RudderOption()
-//                .putIntegration("MIXPANEL",true)
-//        )
-        rudderClient = RudderClient.getInstance(
+//        StrictMode.ThreadPolicy.Builder()
+//            .detectAll()
+//            .penaltyLog()
+//            .build()
+//            .let { StrictMode.setThreadPolicy(it) }
+//
+//        if (LeakCanary.isInAnalyzerProcess(this)) {
+//            // This process is dedicated to LeakCanary for heap analysis.
+//            // You should not init your app in this process.
+//            return;
+//        }
+//        LeakCanary.install(this);
+
+rudderClient = RudderClient.getInstance(
             this,
             WRITE_KEY,
             RudderConfig.Builder()
                 .withDataPlaneUrl(DATA_PLANE_URL)
-                .withLogLevel(RudderLogger.RudderLogLevel.DEBUG)
-                .withTrackLifecycleEvents(false)
-                .withRecordScreenViews(false)
-                .withCustomFactory(CustomFactory.FACTORY)
+                .withLogLevel(RudderLogger.RudderLogLevel.NONE)
+                .withTrackLifecycleEvents(true)
+                .withRecordScreenViews(true)
                 .build()
-)
-        rudderClient!!.putDeviceToken("some_device_token")
-        rudderClient!!.track("first_event")
-
-        Handler().postDelayed({
-            RudderClient.updateWithAdvertisingId("some_idfa_changed")
-            rudderClient!!.track("second_event")
-        }, 3000)
+        )
     }
 }
