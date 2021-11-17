9e68ac20b1121707f4042a7bbba2a80a15e27e61
Desu Sai Venkat <saivenkatdesu@gmail.com>
2021-11-17 16:10:33 +0530

    Added Support for Api Level 19

--- sample-kotlin/src/main/java/com/rudderstack/android/sample/kotlin/MainActivity.kt
+++ sample-kotlin/src/main/java/com/rudderstack/android/sample/kotlin/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.rudderstack.android.sample.kotlin
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException
+import com.google.android.gms.common.GooglePlayServicesRepairableException
+import com.google.android.gms.security.ProviderInstaller
 import com.rudderstack.android.sdk.core.RudderClient
 import com.rudderstack.android.sdk.core.RudderOption
 import com.rudderstack.android.sdk.core.RudderProperty
 import com.rudderstack.android.sdk.core.RudderTraits
-import java.lang.annotation.Native
+import javax.net.ssl.SSLContext
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +22,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT)
+            tlsBackport()
         val option = RudderOption()
             .putExternalId("brazeExternalId", "some_external_id_1")
             .putExternalId("braze_id", "some_braze_id_2")
@@ -40,17 +47,39 @@ class MainActivity : AppCompatActivity() {
 
         MainApplication.rudderClient!!.track("Test Event")
 
-        MainApplication.rudderClient!!.onIntegrationReady("App Center", NativeCallBack("App Center"));
+        MainApplication.rudderClient!!.onIntegrationReady(
+            "App Center",
+            NativeCallBack("App Center")
+        );
 
-        MainApplication.rudderClient!!.onIntegrationReady("Custom Factory", NativeCallBack("Custom Factory"));
+        MainApplication.rudderClient!!.onIntegrationReady(
+            "Custom Factory",
+            NativeCallBack("Custom Factory")
+        );
+    }
 
-
-
+    private fun tlsBackport() {
+        try {
+            ProviderInstaller.installIfNeeded(this)
+            Log.e("Rudder", "Play present")
+            val sslContext: SSLContext = SSLContext.getInstance("TLSv1.2")
+            sslContext.init(null, null, null)
+            sslContext.createSSLEngine()
+        } catch (e: GooglePlayServicesRepairableException) {
+            // Prompt the user to install/update/enable Google Play services.
+            GoogleApiAvailability.getInstance()
+                .showErrorNotification(this, e.connectionStatusCode)
+            Log.e("Rudder", "Play install")
+        } catch (e: GooglePlayServicesNotAvailableException) {
+            // Indicates a non-recoverable error: let the user know.
+            Log.e("SecurityException", "Google Play Services not available.");
+            e.printStackTrace()
+        }
     }
 }
 
 internal class NativeCallBack(private val integrationName: String) : RudderClient.Callback {
     override fun onReady(instance: Any) {
-        println("Call back of integration : "+integrationName +" is called");
+        println("Call back of integration : " + integrationName + " is called");
     }
 }
