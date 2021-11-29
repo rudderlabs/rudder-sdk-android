d39246efa860eb0e92e2674f84fd46299ba9cf0f
Debanjan Chatterjee <debanjanchatterjee99@gmail.com>
2021-11-29 13:26:15 +0530

    changed to content providers instead of accessing database directly. Multiple process synchronization

---
+++ core/src/main/java/com/rudderstack/android/sdk/core/EventContentProvider.java
@@ -0,0 +1,118 @@
+package com.rudderstack.android.sdk.core;
+
+import static android.provider.Telephony.BaseMmsColumns.MESSAGE_ID;
+import static com.rudderstack.android.sdk.core.EventsDbHelper.EVENTS_TABLE_NAME;
+import static com.rudderstack.android.sdk.core.EventsDbHelper.UPDATED;
+
+import android.app.Application;
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.text.TextUtils;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.Locale;
+
+public class EventContentProvider extends ContentProvider {
+    static final String AUTHORITY =
+            EventContentProvider.class.getCanonicalName();
+
+    static final Uri CONTENT_URI_EVENTS =
+            Uri.parse("content://" + AUTHORITY +"/"+ EVENTS_TABLE_NAME);
+
+    static final UriMatcher uriMatcher;
+    static final String QUERY_PARAMETER_LIMIT = "limit";
+    private final static int EVENT_CODE = 1;
+    private final static int EVENT_ID_CODE = 2;
+    static{
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(AUTHORITY, EVENTS_TABLE_NAME, EVENT_CODE);
+        uriMatcher.addURI(AUTHORITY, EVENTS_TABLE_NAME + "/#", EVENT_ID_CODE);
+    }
+
+    private EventsDbHelper dbHelper;
+    @Override
+    public boolean onCreate() {
+        dbHelper = new EventsDbHelper(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        String limit = uri.getQueryParameter(QUERY_PARAMETER_LIMIT);
+        return dbHelper.getWritableDatabase().query(EVENTS_TABLE_NAME,	projection,	selection,
+                selectionArgs,null, null, sortOrder, limit);
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        long rowID = dbHelper.getWritableDatabase().insert(	EVENTS_TABLE_NAME, "", values);
+
+        /**
+         * If record is added successfully
+         */
+        if (rowID > 0) {
+            Uri _uri = ContentUris.withAppendedId(CONTENT_URI_EVENTS, rowID);
+            getContext().getContentResolver().notifyChange(_uri, null);
+            return _uri;
+        }
+
+        throw new SQLException("Failed to add a record into " + uri);
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        int count = 0;
+        switch (uriMatcher.match(uri)){
+            case EVENT_CODE:
+                count = dbHelper.getWritableDatabase().delete(EVENTS_TABLE_NAME, selection, selectionArgs);
+                break;
+
+            case EVENT_ID_CODE:
+                String id = uri.getPathSegments().get(1);
+                count = dbHelper.getWritableDatabase().delete( EVENTS_TABLE_NAME, MESSAGE_ID +  " = " + id +
+                                (!TextUtils.isEmpty(selection) ? " AND (" + selection + ')' : ""), selectionArgs);
+                break;
+
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        int count = 0;
+        switch (uriMatcher.match(uri)) {
+            case EVENT_CODE:
+                count = dbHelper.getWritableDatabase().update(EVENTS_TABLE_NAME, values, selection, selectionArgs);
+                break;
+
+            case EVENT_ID_CODE:
+                count = dbHelper.getWritableDatabase().update(EVENTS_TABLE_NAME, values,
+                        MESSAGE_ID + " = " + uri.getPathSegments().get(1) +
+                                (!TextUtils.isEmpty(selection) ? " AND (" +selection + ')' : ""), selectionArgs);
+                break;
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+
+
+}
---
+++ core/src/main/java/com/rudderstack/android/sdk/core/EventsDbHelper.java
@@ -0,0 +1,61 @@
+package com.rudderstack.android.sdk.core;
+
+import android.app.Application;
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+import java.util.Locale;
+
+final class EventsDbHelper extends SQLiteOpenHelper {
+    // SQLite database file name
+    private static final String DB_NAME = "rl_persistence.db";
+    // SQLite database version number
+    private static final int DB_VERSION = 1;
+    static final String EVENTS_TABLE_NAME = "events";
+    static final String MESSAGE = "message";
+     static final String MESSAGE_ID = "id";
+    static final String UPDATED = "updated";
+
+     EventsDbHelper(Context context){
+        this(context, DB_NAME, null, DB_VERSION);
+    }
+    private EventsDbHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        this(context, name, factory, version, null);
+    }
+
+    private EventsDbHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version, @Nullable DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    private EventsDbHelper(@Nullable Context context, @Nullable String name, int version, @NonNull SQLiteDatabase.OpenParams openParams) {
+        super(context, name, version, openParams);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        createSchema(db);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        // basically do nothing
+    }
+    /*
+     * create table initially if not exists
+     * */
+    private void createSchema(SQLiteDatabase db) {
+        String createSchemaSQL = String.format(Locale.US, "CREATE TABLE IF NOT EXISTS '%s' ('%s' INTEGER PRIMARY KEY AUTOINCREMENT, '%s' TEXT NOT NULL, '%s' INTEGER NOT NULL)",
+                EVENTS_TABLE_NAME, MESSAGE_ID, MESSAGE, UPDATED);
+        RudderLogger.logVerbose(String.format(Locale.US, "DBPersistentManager: createSchema: createSchemaSQL: %s", createSchemaSQL));
+        db.execSQL(createSchemaSQL);
+        RudderLogger.logInfo("DBPersistentManager: createSchema: DB Schema created");
+    }
+}
