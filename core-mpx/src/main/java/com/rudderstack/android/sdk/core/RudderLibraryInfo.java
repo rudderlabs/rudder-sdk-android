e498b12ee95c618e2a5197803473b5476ce85ad6
Desu Sai Venkat <saivenkatdesu@gmail.com>
2021-11-30 17:54:42 +0530

    Updated Package name

--- core-mpx/src/main/java/com/rudderstack/android/sdk/core/RudderLibraryInfo.java
+++ core-mpx/src/main/java/com/rudderstack/android/sdk/core/RudderLibraryInfo.java
@@ -1,10 +1,11 @@
 package com.rudderstack.android.sdk.core;
 
 import com.google.gson.annotations.SerializedName;
+import com.rudderstack.android.sdk.corempx.BuildConfig;
 
 class RudderLibraryInfo {
     @SerializedName("name")
     private String name = BuildConfig.LIBRARY_PACKAGE_NAME;
     @SerializedName("version")
-    private String version = "1.2.1";
+    private String version = "1.0.0";
 }
