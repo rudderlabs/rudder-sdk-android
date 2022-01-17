bd59c39b3b0baa5453d09816f3aba3abc709baf6
1abhishekpandey <abhishekpandey10196@gmail.com>
2022-01-17 22:14:55 +0530

    Implemented unit test cases

--- core/src/test/java/com/rudderstack/android/sdk/core/RudderEventFilteringPluginTest.java
+++ core/src/test/java/com/rudderstack/android/sdk/core/RudderEventFilteringPluginTest.java
@@ -4,28 +4,19 @@ import static com.rudderstack.android.sdk.core.RudderEventFilteringPlugin.BLACKL
 import static com.rudderstack.android.sdk.core.RudderEventFilteringPlugin.DISABLE;
 import static com.rudderstack.android.sdk.core.RudderEventFilteringPlugin.EVENT_FILTERING_OPTION;
 import static com.rudderstack.android.sdk.core.RudderEventFilteringPlugin.WHITELISTED_EVENTS;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mockStatic;
-import static org.mockito.Mockito.withSettings;
 
 import android.text.TextUtils;
 import android.util.Log;
 
 import com.rudderstack.android.sdk.core.util.Utils;
 
-import org.hamcrest.Matchers.*;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentMatchers;
-import org.mockito.Mock;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import org.powermock.api.mockito.PowerMockito;
@@ -38,133 +29,216 @@ import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Nullable;
+
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({TextUtils.class, Log.class})
 public class RudderEventFilteringPluginTest {
-    private final List<RudderServerDestination> testDestinations = new ArrayList();
-
-    private RudderEventFilteringPlugin plugin;
 
 
+    private RudderEventFilteringPlugin plugin_Blacklist;
+    private RudderEventFilteringPlugin plugin_Whitelist;
+    private RudderEventFilteringPlugin plugin_Disable;
+    private RudderEventFilteringPlugin getPlugin_Whitelist_LifeCycle;
+    private RudderEventFilteringPlugin getPlugin_Blacklist_LifeCycle;
 
     @Before
     public void setup() {
-        List<Map<String, String>> blackListedEvents1 = new ArrayList<>();
-        Map<String, String> event1_1 = new HashMap<>();
-        event1_1.put("eventName", "w_e_1_1");
-        Map<String, String> event1_2 = new HashMap<>();
-        event1_2.put("eventName", "w_e_1_2");
-        Map<String, String> event1_3 = new HashMap<>();
-        event1_3.put("eventName", "w_e_1_3");
-        Map<String, String> event1_4 = new HashMap<>();
-        event1_4.put("eventName", "w_e_1_4");
-
-        blackListedEvents1.add(event1_1);
-        blackListedEvents1.add(event1_2);
-        blackListedEvents1.add(event1_3);
-        blackListedEvents1.add(event1_4);
-
-        List<Map<String, String>> whiteListedEvents2 = new ArrayList<>();
-        Map<String, String> event2_1 = new HashMap<>();
-        event2_1.put("eventName", "w_e_2_1");
-        Map<String, String> event2_2 = new HashMap<>();
-        event2_2.put("eventName", "w_e_2_2");
-        Map<String, String> event2_3 = new HashMap<>();
-        event2_3.put("eventName", "w_e_2_3");
-        Map<String, String> event2_4 = new HashMap<>();
-        event2_4.put("eventName", "w_e_2_4");
-
-        whiteListedEvents2.add(event2_1);
-        whiteListedEvents2.add(event2_2);
-        whiteListedEvents2.add(event2_3);
-        whiteListedEvents2.add(event2_4);
-
-        testDestinations.add(createServerDestination("1", "D-1",
-                true, "d-1",
-                "ddd-1", EventFilteringStatus.BLACK_LISTED,
-                null, blackListedEvents1
-        ));
-        testDestinations.add(createServerDestination("2", "D-2",
-                true, "D-2",
-                "ddd-2", EventFilteringStatus.WHITE_LISTED,
-                 whiteListedEvents2, null
-        ));
-
         mockTheLog();
         PowerMockito.mockStatic(TextUtils.class);
-//        mockTextUtils = mockStatic(TextUtils.class);
 
+        final List<RudderServerDestination> testDestinations_Amplitude_BlacklistedEvents = new ArrayList();
+        final List<RudderServerDestination> testDestinations_Amplitude_WhitelistedEvents = new ArrayList();
+        final List<RudderServerDestination> testDestinations_Amplitude_Disable = new ArrayList();
+
+        List<Map<String, String>> blackListedEvents = new ArrayList<>();
+        Map<String, String> event1_1 = new HashMap<>();
+        event1_1.put("eventName", "Black-1");
+        Map<String, String> event1_2 = new HashMap<>();
+        event1_2.put("eventName", "Black-2");
+        Map<String, String> event1_3 = new HashMap<>();
+        event1_3.put("eventName", "Black-3");
+        Map<String, String> event1_4 = new HashMap<>();
+        event1_4.put("eventName", "Black-4");
+
+        blackListedEvents.add(event1_1);
+        blackListedEvents.add(event1_2);
+        blackListedEvents.add(event1_3);
+        blackListedEvents.add(event1_4);
+
+        List<Map<String, String>> whiteListedEvents = new ArrayList<>();
+        Map<String, String> event2_1 = new HashMap<>();
+        event2_1.put("eventName", "White-1");
+        Map<String, String> event2_2 = new HashMap<>();
+        event2_2.put("eventName", "White-2");
+        Map<String, String> event2_3 = new HashMap<>();
+        event2_3.put("eventName", "White-3");
+        Map<String, String> event2_4 = new HashMap<>();
+        event2_4.put("eventName", "White-4");
+
+        whiteListedEvents.add(event2_1);
+        whiteListedEvents.add(event2_2);
+        whiteListedEvents.add(event2_3);
+        whiteListedEvents.add(event2_4);
+
+        List<Map<String, String>> lifeCycleEvents = new ArrayList<>();
+        Map<String, String> event3_1 = new HashMap<>();
+        event3_1.put("eventName", "White-1");
+        Map<String, String> event3_2 = new HashMap<>();
+        event3_2.put("eventName", "White-2");
+        Map<String, String> event3_3 = new HashMap<>();
+        event3_3.put("eventName", "White-3");
+        Map<String, String> event3_4 = new HashMap<>();
+        event3_4.put("eventName", "White-4");
+
+        lifeCycleEvents.add(event3_1);
+        lifeCycleEvents.add(event3_2);
+        lifeCycleEvents.add(event3_3);
+        lifeCycleEvents.add(event3_4);
+
+        // Blacklisted
+        testDestinations_Amplitude_BlacklistedEvents.add(createServerDestination("23YLpuMKgF5t5U6Ac7HcY5seOIr", "Amplitude Event Filtering",
+                true, "AM",
+                "Amplitude", EventFilteringStatus.BLACK_LISTED,
+                null, blackListedEvents
+        ));
+        // Whitelisted
+        testDestinations_Amplitude_WhitelistedEvents.add(createServerDestination("23YLpuMKgF5t5U6Ac7HcY5seOIr", "Amplitude Event Filtering",
+                true, "AM",
+                "Amplitude", EventFilteringStatus.WHITE_LISTED, whiteListedEvents, null
+        ));
+        // Disable
+        testDestinations_Amplitude_Disable.add(createServerDestination("23YLpuMKgF5t5U6Ac7HcY5seOIr", "Amplitude Event Filtering",
+                true, "AM",
+                "Amplitude", EventFilteringStatus.DISABLE,
+                null, blackListedEvents
+        ));
+
+//        testDestinations_Amplitude_BlacklistedEvents.add(createServerDestination("2", "D-2",
+//                true, "d-2",
+//                "ddd-2", EventFilteringStatus.WHITE_LISTED,
+//                 whiteListedEvents, null
+//        ));
+
+
+        // For any string values
         PowerMockito.when(TextUtils.isEmpty(any(CharSequence.class))).thenAnswer(new Answer<Boolean>() {
             @Override
-            public Boolean answer(InvocationOnMock invocation) throws Throwable {
-
+            public Boolean answer(InvocationOnMock invocation) {
                 CharSequence a = (CharSequence) invocation.getArguments()[0];
-
                 return !(a != null && a.length() > 0);
-
+            }
+        });
+        // For null values
+        PowerMockito.when(TextUtils.isEmpty(null)).thenAnswer(new Answer<Boolean>() {
+            @Override
+            public Boolean answer(InvocationOnMock invocation) {
+                return true;
             }
         });
 
-        plugin = new RudderEventFilteringPlugin(testDestinations);
-
+        plugin_Blacklist = new RudderEventFilteringPlugin(testDestinations_Amplitude_BlacklistedEvents);
+        plugin_Whitelist = new RudderEventFilteringPlugin(testDestinations_Amplitude_WhitelistedEvents);
+        plugin_Disable = new RudderEventFilteringPlugin(testDestinations_Amplitude_Disable);
     }
 
     private void mockTheLog() {
         PowerMockito.mockStatic(Log.class);
         PowerMockito.when(Log.d(anyString(), anyString())).thenAnswer(new Answer<Integer>() {
             @Override
-            public Integer answer(InvocationOnMock invocation) throws Throwable {
+            public Integer answer(InvocationOnMock invocation) {
                 return 0;
             }
         });
         PowerMockito.when(Log.e(anyString(), anyString())).thenAnswer(new Answer<Integer>() {
             @Override
-            public Integer answer(InvocationOnMock invocation) throws Throwable {
+            public Integer answer(InvocationOnMock invocation) {
                 return 0;
             }
         });
         PowerMockito.when(Log.w(anyString(), anyString())).thenAnswer(new Answer<Integer>() {
             @Override
-            public Integer answer(InvocationOnMock invocation) throws Throwable {
+            public Integer answer(InvocationOnMock invocation) {
                 return 0;
             }
         });
         PowerMockito.when(Log.i(anyString(), anyString())).thenAnswer(new Answer<Integer>() {
             @Override
-            public Integer answer(InvocationOnMock invocation) throws Throwable {
+            public Integer answer(InvocationOnMock invocation) {
                 return 0;
             }
         });
         PowerMockito.when(Log.v(anyString(), anyString())).thenAnswer(new Answer<Integer>() {
             @Override
-            public Integer answer(InvocationOnMock invocation) throws Throwable {
+            public Integer answer(InvocationOnMock invocation) {
                 return 0;
             }
         });
         PowerMockito.when(Log.wtf(anyString(), anyString())).thenAnswer(new Answer<Integer>() {
             @Override
-            public Integer answer(InvocationOnMock invocation) throws Throwable {
+            public Integer answer(InvocationOnMock invocation) {
                 return 0;
             }
         });
     }
 
-    @After
-     public void destroy(){
-//         mockTextUtils.close();
-     }
-
-
     @Test
     public void isEventAllowed() throws Exception {
+        RudderMessage message = PowerMockito.mock(RudderMessage.class);
 
-        RudderMessage msg =
-                PowerMockito.mock(RudderMessage.class);
-        PowerMockito.when(msg,"getEventName").thenReturn("w_e_1_1");
-        PowerMockito.when(msg,"getType" ).thenReturn(MessageType.TRACK);
-//        msg.setEventName();
-//        msg.setType(MessageType.TRACK);
-        assertThat(plugin.isEventAllowed("ddd-1", msg), is(false));
+        // When event is blocked or not whitelisted -> Do not allow the event
+        PowerMockito.when(message,"getEventName").thenReturn("Black-1");
+        PowerMockito.when(message,"getType").thenReturn(MessageType.TRACK);
+        assertThat(plugin_Blacklist.isEventAllowed("Amplitude", message), is(false));
+
+        PowerMockito.when(message,"getEventName").thenReturn("AsNotWhitelisted-EventBlocked");
+        PowerMockito.when(message,"getType").thenReturn(MessageType.TRACK);
+        assertThat(plugin_Whitelist.isEventAllowed("Amplitude", message), is(false));
+
+        // When event is whitelisted or not blacked -> Allow the event
+        PowerMockito.when(message,"getEventName").thenReturn("AsNotBlacklisted-EventAllowed");
+        PowerMockito.when(message,"getType").thenReturn(MessageType.TRACK);
+        assertThat(plugin_Blacklist.isEventAllowed("Amplitude", message), is(true));
+
+        PowerMockito.when(message,"getEventName").thenReturn("White-1");
+        PowerMockito.when(message,"getType").thenReturn(MessageType.TRACK);
+        assertThat(plugin_Whitelist.isEventAllowed("Amplitude", message), is(true));
+
+        // When message.getType() == null or empty -> Allow the event (If message.type is undetermined)
+        PowerMockito.when(message,"getEventName").thenReturn("CouldBeAnything");
+        PowerMockito.when(message,"getType").thenReturn(null);
+        assertThat(plugin_Whitelist.isEventAllowed("Amplitude", message), is(true));
+
+
+        PowerMockito.when(message,"getEventName").thenReturn("CouldBeAnything");
+        PowerMockito.when(message,"getType").thenReturn("");
+        assertThat(plugin_Whitelist.isEventAllowed("Amplitude", message), is(true));
+
+        // When message.getEventName == null -> Allow the event (If message.getEventName is null
+        PowerMockito.when(message,"getEventName").thenReturn(null);
+        PowerMockito.when(message,"getType").thenReturn(MessageType.TRACK);
+        assertThat(plugin_Whitelist.isEventAllowed("Amplitude", message), is(true));
+
+        PowerMockito.when(message,"getEventName").thenReturn("");
+        PowerMockito.when(message,"getType").thenReturn(MessageType.TRACK);
+        assertThat(plugin_Whitelist.isEventAllowed("Amplitude", message), is(true));
+
+        // When eventFilterOption is disable (backward compatibility) -> Allow the event
+        PowerMockito.when(message,"getEventName").thenReturn("CouldBeAnything");
+        PowerMockito.when(message,"getType").thenReturn(MessageType.TRACK);
+        assertThat(plugin_Disable.isEventAllowed("Amplitude", message), is(true));
+        assertThat(plugin_Whitelist.isEventAllowed("Adobe", message), is(true));
+        assertThat(plugin_Blacklist.isEventAllowed("Adobe", message), is(true));
+
+        // Event name with white spaces -> If the event is whitelisted or not blacklisted then allow the event.
+        PowerMockito.when(message,"getEventName").thenReturn("    AsNotBlacklisted-EventAllowed     ");
+        PowerMockito.when(message,"getType").thenReturn(MessageType.TRACK);
+        assertThat(plugin_Blacklist.isEventAllowed("Amplitude", message), is(true));
+
+        PowerMockito.when(message,"getEventName").thenReturn("      White-1         ");
+        PowerMockito.when(message,"getType").thenReturn(MessageType.TRACK);
+        assertThat(plugin_Whitelist.isEventAllowed("Amplitude", message), is(true));
+
+
 
         /*RudderMessage msg2 = PowerMockito.mock(RudderMessage.class);
         msg2.setEventName("w_e_1_2");
@@ -225,7 +299,6 @@ public class RudderEventFilteringPluginTest {
 
     private RudderServerDestination createServerDestination(String destinationId, String destinationName, boolean isDestinationEnabled,
                                                             String destinationDefinitionName, String destinationDefinitionDisplayName,
-//                                                            Map<String, Object> destinationConfig,
                                                             EventFilteringStatus eventFilteringStatus,
                                                             @Nullable List<Map<String, String>> whiteListedEvents,
                                                             @Nullable List<Map<String, String>> blackListedEvents) {
@@ -254,7 +327,7 @@ public class RudderEventFilteringPluginTest {
         return destination;
     }
 
-    static enum EventFilteringStatus {
+    enum EventFilteringStatus {
         DISABLE,
         WHITE_LISTED,
         BLACK_LISTED
