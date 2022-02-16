d87fa768e37a31a938de71e7acbcccdbec52c11b
Desu Sai Venkat <saivenkatdesu@gmail.com>
2022-02-16 18:40:23 +0530

    Bumped version to 1.4.0

--- core/src/main/java/com/rudderstack/android/sdk/core/RudderLibraryInfo.java
+++ core/src/main/java/com/rudderstack/android/sdk/core/RudderLibraryInfo.java
@@ -6,5 +6,5 @@ class RudderLibraryInfo {
     @SerializedName("name")
     private String name = BuildConfig.LIBRARY_PACKAGE_NAME;
     @SerializedName("version")
-    private String version = "1.2.1";
+    private String version = "1.4.0";
 }
