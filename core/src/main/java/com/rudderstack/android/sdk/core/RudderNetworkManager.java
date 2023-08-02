e2db5f2ad5caef48a7b9d45d03204397b04b8905
Debanjan Chatterjee <debanjanchatterjee99@gmail.com>
2023-08-02 17:33:48 +0530

    fix: added an import

--- core/src/main/java/com/rudderstack/android/sdk/core/RudderNetworkManager.java
+++ core/src/main/java/com/rudderstack/android/sdk/core/RudderNetworkManager.java
@@ -15,6 +15,7 @@ import com.rudderstack.android.sdk.core.util.MessageUploadLock;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
