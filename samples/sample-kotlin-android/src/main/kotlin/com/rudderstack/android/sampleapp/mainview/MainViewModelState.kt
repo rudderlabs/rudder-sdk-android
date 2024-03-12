ccd49dee987c5ee318d0e8990e9316cd8a7449df
Debanjan Chatterjee <debanjanchatterjee99@gmail.com>
2024-03-12 10:50:27 +0530

    chore: updated sample to use local.properties. feat: added secondary analytics instance. feat: added buttons for secondary analytics.
    Signed-off-by: Debanjan Chatterjee <debanjanchatterjee99@gmail.com>

--- samples/sample-kotlin-android/src/main/kotlin/com/rudderstack/android/sampleapp/mainview/MainViewModelState.kt
+++ samples/sample-kotlin-android/src/main/kotlin/com/rudderstack/android/sampleapp/mainview/MainViewModelState.kt
@@ -10,7 +10,7 @@ data class MainViewModelState(
 data class LogData(val time: Date, val log: String)
 sealed class AnalyticsState(val eventName: String) {
     object ClearAnalytics : AnalyticsState("Clear")
-    object InitializeAnalytics : AnalyticsState("Initialize")
+//    object InitializeAnalytics : AnalyticsState("Initialize")
     object ShutDownAnalytics : AnalyticsState("ShutDown")
     object TrackEvent : AnalyticsState("Track")
     object IdentifyEvent : AnalyticsState("Identify")
@@ -23,5 +23,18 @@ sealed class AnalyticsState(val eventName: String) {
     object EnableAutoTracking : AnalyticsState("Enable Auto Tracking")
     object DisableAutoTracking : AnalyticsState("Disable Auto Tracking")
     object ForceFlush : AnalyticsState("Force Flush")
+
+    object ShutDownAnalyticsSecondary : AnalyticsState("ShutDown Secondary")
+    object TrackEventSecondary : AnalyticsState("Track Secondary")
+    object IdentifyEventSecondary : AnalyticsState("Identify Secondary")
+    object AliasEventSecondary : AnalyticsState("Alias Secondary")
+    object GroupEventSecondary : AnalyticsState("Group Secondary")
+    object ScreenEventSecondary : AnalyticsState("Screen Secondary")
+    object OptInAnalyticsSecondary : AnalyticsState("Opt In/Out Secondary")
+    object StartManualSessionSecondary : AnalyticsState("start manual session Secondary")
+    object EndSessionSecondary : AnalyticsState("End Session Secondary")
+    object EnableAutoTrackingSecondary : AnalyticsState("Enable Auto Tracking Secondary")
+    object DisableAutoTrackingSecondary : AnalyticsState("Disable Auto Tracking Secondary")
+    object ForceFlushSecondary : AnalyticsState("Force Flush Secondary")
     object SendError : AnalyticsState("Send Error")
 }
