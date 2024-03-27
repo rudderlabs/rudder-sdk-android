015fd984d59ee33565bbe6c12b2461fb96c18626
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-03-27 21:37:55 +0530

    refactor: move the lifecycle logic to AppInstallUpdateTrackerPlugin.kt

--- android/src/main/java/com/rudderstack/android/internal/infrastructure/AppInstallUpdateTrackerPlugin.kt
+++ android/src/main/java/com/rudderstack/android/internal/infrastructure/AppInstallUpdateTrackerPlugin.kt
@@ -1,65 +1,96 @@
 package com.rudderstack.android.internal.infrastructure
 
-import android.app.Application
+import android.content.pm.PackageManager
+import android.os.Build
 import com.rudderstack.android.androidStorage
 import com.rudderstack.android.currentConfigurationAndroid
 import com.rudderstack.android.storage.AndroidStorage
 import com.rudderstack.models.AppVersion
-import com.rudderstack.android.utilities.AppVersionManager
 import com.rudderstack.core.Analytics
 import com.rudderstack.core.InfrastructurePlugin
-import com.rudderstack.core.Logger
+
+private const val PREVIOUS_VERSION = "previous_version"
+private const val PREVIOUS_BUILD = "previous_build"
+private const val VERSION = "version"
+private const val BUILD = "build"
+
+private const val EVENT_NAME_APPLICATION_INSTALLED = "Application Installed"
+private const val EVENT_NAME_APPLICATION_UPDATED = "Application Updated"
+
+private const val DEFAULT_BUILD = -1
+private const val DEFAULT_VERSION_NAME = ""
 
 class AppInstallUpdateTrackerPlugin : InfrastructurePlugin {
 
     private var analytics: Analytics? = null
     private lateinit var appVersion: AppVersion
 
-    companion object {
-        private const val PREVIOUS_VERSION = "previous_version"
-        private const val PREVIOUS_VERSION_CODE = "previous_build"
-        private const val VERSION = "version"
-        private const val VERSION_CODE = "build"
-
-        const val EVENT_NAME_APPLICATION_INSTALLED = "Application Installed"
-        const val EVENT_NAME_APPLICATION_UPDATED = "Application Updated"
-    }
-
     override fun setup(analytics: Analytics) {
         this.analytics = analytics
-        initialiseAppVersion(analytics.androidStorage, analytics.logger, analytics.currentConfigurationAndroid?.application)
+        this.appVersion = getAppVersion(analytics)
+        storeVersionNameAndCode(analytics.androidStorage)
         if (this.analytics?.currentConfigurationAndroid?.trackLifecycleEvents == true) {
             trackApplicationStatus()
         }
     }
 
-    private fun initialiseAppVersion(analyticsStorage: AndroidStorage, logger: Logger, application: Application?) {
-        val appVersionManager = AppVersionManager(application, analyticsStorage, logger)
-        appVersionManager.updateAppVersionInStorage()
-        this.appVersion = appVersionManager.getAppVersionInfo()
+    private fun getAppVersion(analytics: Analytics): AppVersion {
+        val previousBuild: Int? = analytics.androidStorage.build
+        val previousVersionName: String? = analytics.androidStorage.versionName
+        var currentBuild: Int? = null
+        var currentVersionName: String? = null
+
+        try {
+            val packageName = analytics.currentConfigurationAndroid?.application?.packageName
+            val packageManager: PackageManager? = analytics.currentConfigurationAndroid?.application?.packageManager
+            val packageInfo = packageName?.let {
+                packageManager?.getPackageInfo(it, 0)
+            }
+
+            currentVersionName = packageInfo?.versionName
+            currentBuild = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                packageInfo?.longVersionCode?.toInt()
+            } else {
+                packageInfo?.versionCode
+            }
+        } catch (ex: PackageManager.NameNotFoundException) {
+            analytics.logger.error(log = "Failed to get app version info: ${ex.message}")
+        }
+
+        return AppVersion(
+            previousBuild = previousBuild ?: DEFAULT_BUILD,
+            previousVersionName = previousVersionName ?: DEFAULT_VERSION_NAME,
+            currentBuild = currentBuild ?: DEFAULT_BUILD,
+            currentVersionName = currentVersionName ?: DEFAULT_VERSION_NAME,
+        )
+    }
+
+    private fun storeVersionNameAndCode(analyticsStorage: AndroidStorage) {
+        this.appVersion.currentVersionName.let { analyticsStorage.setVersionName(it) }
+        this.appVersion.currentBuild.let { analyticsStorage.setBuild(it) }
     }
 
     private fun trackApplicationStatus() {
         if (this.isApplicationInstalled()) {
             sendApplicationInstalledEvent()
-        } else if(this.isApplicationUpdated()) {
+        } else if (this.isApplicationUpdated()) {
             sendApplicationUpdatedEvent()
         }
     }
 
     private fun isApplicationInstalled(): Boolean {
-        return this.appVersion.previousVersionCode == -1
+        return this.appVersion.previousBuild == -1
     }
 
     private fun isApplicationUpdated(): Boolean {
-        return this.appVersion.previousVersionCode != -1 && this.appVersion.previousVersionCode != this.appVersion.currentVersionCode
+        return this.appVersion.previousBuild != -1 && this.appVersion.previousBuild != this.appVersion.currentBuild
     }
 
     private fun sendApplicationInstalledEvent() {
         this.analytics?.logger?.debug(log = "Tracking Application Installed event")
         val trackProperties = mutableMapOf<String, Any>()
         trackProperties[VERSION] = this.appVersion.currentVersionName
-        trackProperties[VERSION_CODE] = this.appVersion.currentVersionCode
+        trackProperties[BUILD] = this.appVersion.currentBuild
 
         sendEvent(EVENT_NAME_APPLICATION_INSTALLED, trackProperties)
     }
@@ -68,9 +99,9 @@ class AppInstallUpdateTrackerPlugin : InfrastructurePlugin {
         this.analytics?.logger?.debug(log = "Tracking Application Updated event")
         val trackProperties = mutableMapOf<String, Any>()
         trackProperties[PREVIOUS_VERSION] = this.appVersion.previousVersionName
-        trackProperties[PREVIOUS_VERSION_CODE] = this.appVersion.previousVersionCode
+        trackProperties[PREVIOUS_BUILD] = this.appVersion.previousBuild
         trackProperties[VERSION] = this.appVersion.currentVersionName
-        trackProperties[VERSION_CODE] = this.appVersion.currentVersionCode
+        trackProperties[BUILD] = this.appVersion.currentBuild
 
         sendEvent(EVENT_NAME_APPLICATION_UPDATED, trackProperties)
     }
