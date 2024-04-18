48912e39e5d48cfd814163b8ef284d856b2e2a67
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-04-18 17:30:56 +0530

    test(CentralPluginChainTest): remove commented out codes

--- core/src/test/java/com/rudderstack/core/CentralPluginChainTest.kt
+++ core/src/test/java/com/rudderstack/core/CentralPluginChainTest.kt
@@ -20,11 +20,9 @@ import com.rudderstack.models.TrackMessage
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
 import org.hamcrest.Matchers.`is`
-import org.hamcrest.Matchers.not
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.any
@@ -32,8 +30,6 @@ import org.mockito.kotlin.argumentCaptor
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
-import java.util.*
-import java.util.concurrent.atomic.AtomicInteger
 
 /**
  * Testing flow of control through plugins.
@@ -44,16 +40,6 @@ import java.util.concurrent.atomic.AtomicInteger
 @RunWith(MockitoJUnitRunner::class)
 class CentralPluginChainTest {
 
-    companion object{
-        //Plugins should be called in this order
-        private const val SUB_PLUGIN_1_1_SL_NO = 1
-        private const val SUB_PLUGIN_2_1_SL_NO = 2
-        private const val MAIN_PLUGIN_1_SL_NO = 3
-        private const val SUB_PLUGIN_1_2_SL_NO = 4
-        private const val MAIN_PLUGIN_2_SL_NO = 5
-        private const val MAIN_PLUGIN_3_SL_NO = 6
-        private const val TOTAL_NO_OF_CALLS = 7
-    }
     @Mock
     lateinit var mockPlugin1: Plugin
 
@@ -78,39 +64,18 @@ class CentralPluginChainTest {
 
     private lateinit var centralPluginChain: CentralPluginChain
 
-    /*private val dummyPlugin1  = BaseDestinationPlugin<Any>("dummy1") {
-        val msg = it.message()
-        assertThat(enteredPluginNo ++, `is`(MAIN_PLUGIN_1_SL_NO))
-        //message and original message should be different for destination plugin
-        assertThat(msg, not(it.originalMessage))
-        it.proceed(it.originalMessage)
-    }
-    private val dummyPlugin2 = BaseDestinationPlugin<Any>("dummy2") {
-        val msg = it.message()
-        assertThat(enteredPluginNo ++, `is`(MAIN_PLUGIN_2_SL_NO))
-
-        //message and original message should be different for destination plugin
-        assertThat(msg, not(it.originalMessage))
-        //destination plugins should always call proceed on original message to discard changes.
-        it.proceed(it.originalMessage)
-    }
-    private val dummyPlugin3 = Plugin {
-        val msg = it.message()
-        assertThat(enteredPluginNo ++, `is`(MAIN_PLUGIN_3_SL_NO))
-        it.proceed(msg)
-    }*/
     @Before
-    fun setup(){
+    fun setup() {
         // Mock the behavior of plugins and destination plugin
-        whenever(mockPlugin1.intercept(any())).thenAnswer{
+        whenever(mockPlugin1.intercept(any())).thenAnswer {
             val chain = it.arguments[0] as CentralPluginChain
             chain.proceed(mockMessage)
         }
-        whenever(mockPlugin2.intercept(any())).thenAnswer{
+        whenever(mockPlugin2.intercept(any())).thenAnswer {
             val chain = it.arguments[0] as CentralPluginChain
             chain.proceed(mockMessage)
         }
-        whenever(mockDestinationPlugin.intercept(any())).thenAnswer{
+        whenever(mockDestinationPlugin.intercept(any())).thenAnswer {
             val chain = it.arguments[0] as CentralPluginChain
             chain.proceed(mockMessage)
         }
@@ -119,6 +84,7 @@ class CentralPluginChainTest {
         centralPluginChain = CentralPluginChain(mockMessage, plugins, originalMessage = mockMessage)
 //        whenever(mockMessage.copy()).thenReturn(mockMessage)
     }
+
     @Test
     fun testMessage() {
         assertThat(centralPluginChain.message(), equalTo(mockMessage))
@@ -126,8 +92,6 @@ class CentralPluginChainTest {
 
     @Test
     fun testProceed() {
-
-
         // Call the method under test
         val resultMessage = centralPluginChain.proceed(mockMessage)
 
@@ -158,21 +122,21 @@ class CentralPluginChainTest {
 
     @Test(expected = IllegalStateException::class)
     fun testProceedTwice() {
-
         // Call the method under test twice
         centralPluginChain.proceed(mockMessage)
         centralPluginChain.proceed(mockMessage)
     }
+
     @Test
-    fun testCentralPluginChainWithSubPlugins(){
+    fun testCentralPluginChainWithSubPlugins() {
         val subPlugin1 = mock<DestinationPlugin.DestinationInterceptor>()
-        whenever(subPlugin1.intercept(any())).thenAnswer{
+        whenever(subPlugin1.intercept(any())).thenAnswer {
             val chain = it.arguments[0] as CentralPluginChain
             chain.proceed(mockMessage)
         }
         val subPlugin2 = mock<DestinationPlugin.DestinationInterceptor>()
         // Mock the behavior of plugins and destination plugin
-        whenever(subPlugin2.intercept(any())).thenAnswer{
+        whenever(subPlugin2.intercept(any())).thenAnswer {
             val chain = it.arguments[0] as CentralPluginChain
             chain.proceed(mockMessage)
         }
@@ -194,18 +158,4 @@ class CentralPluginChainTest {
         assertThat(chain2.plugins.size, `is`(2)) //number of plugins should be 2 for sub plugins
         assertThat(chain2.originalMessage, equalTo(mockMessage))
     }
-
-    /*@Test
-    fun testCentralPluginChain(){
-        val dummyMsg = TrackMessage.create("message_id",
-            anonymousId = "anon_id", timestamp = Date().toString())
-        CentralPluginChain(dummyMsg, listOf(
-            dummyPlugin1, dummyPlugin2, dummyPlugin3) )
-            .proceed(dummyMsg)
-        //check if all plugins and sub plugins are called
-        assertThat(enteredPluginNo , `is`(TOTAL_NO_OF_CALLS))
-    }
-
-*/
-
 }
