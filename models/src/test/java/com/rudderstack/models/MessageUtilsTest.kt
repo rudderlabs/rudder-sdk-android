e91d49b4bef1ed7f5e0029a8e91056509790db73
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-06-20 00:40:10 +0530

    chore: fix indentation of MessageUtilsTest

--- models/src/test/java/com/rudderstack/models/MessageUtilsTest.kt
+++ models/src/test/java/com/rudderstack/models/MessageUtilsTest.kt
@@ -10,13 +10,13 @@ import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
 import org.junit.Test
 
-class MessageUtilsTest{
+class MessageUtilsTest {
     @Test
     fun `optAdd with both contexts null`() {
         val context1: MessageContext? = null
         val context2: MessageContext? = null
 
-        val result = context1 optAddContext  context2
+        val result = context1 optAddContext context2
 
         assertNull(result)
     }
@@ -26,10 +26,11 @@ class MessageUtilsTest{
         val context1: MessageContext? = null
         val context2: MessageContext = mapOf("key" to "value")
 
-        val result = context1 optAddContext  context2
+        val result = context1 optAddContext context2
 
         assertEquals(context2, result)
     }
+
     @Test
     fun `optAdd with second context null`() {
         val context1: MessageContext = mapOf("key" to "value")
@@ -48,13 +49,15 @@ class MessageUtilsTest{
         val context2: MessageContext = createContext(
             mapOf("trait2" to "value2", "common" to "value2")
         )
-        val result = context1 optAddContext  context2
+        val result = context1 optAddContext context2
         println(result)
-        assertThat(result?.traits, allOf(
-            hasEntry("trait1",  "value1"),
-            hasEntry("trait2",  "value2"),
-            hasEntry("common",  "value1"),
-        ))
+        assertThat(
+            result?.traits, allOf(
+                hasEntry("trait1", "value1"),
+                hasEntry("trait2", "value2"),
+                hasEntry("common", "value1"),
+            )
+        )
     }
 
     @Test
@@ -66,26 +69,31 @@ class MessageUtilsTest{
             Constants.CUSTOM_CONTEXT_MAP_ID to mapOf("context2" to "value2")
         )
 
-        val result = context1 optAddContext  context2
+        val result = context1 optAddContext context2
 
-        assertThat(result?.customContexts, allOf(
-            hasEntry("context1", "value1"),
-            hasEntry("context2", "value2"),
-        ))
+        assertThat(
+            result?.customContexts, allOf(
+                hasEntry("context1", "value1"),
+                hasEntry("context2", "value2"),
+            )
+        )
     }
 
     @Test
     fun `optAdd with overlapping externalIds`() {
         val context1: MessageContext = createContext(
-            externalIds =  listOf(mapOf("id1" to "value1"))
+            externalIds = listOf(mapOf("id1" to "value1"))
         )
         val context2: MessageContext = createContext(
-            externalIds =  listOf(mapOf("id2" to "value2"), mapOf("id1" to "value3"))
+            externalIds = listOf(mapOf("id2" to "value2"), mapOf("id1" to "value3"))
         )
 
-        val result = context1 optAddContext  context2
+        val result = context1 optAddContext context2
 
-        assertThat(result?.externalIds, hasItems(mapOf("id1" to "value1"), mapOf("id2" to "value2"), mapOf("id2" to "value2")))
+        assertThat(
+            result?.externalIds,
+            hasItems(mapOf("id1" to "value1"), mapOf("id2" to "value2"), mapOf("id2" to "value2"))
+        )
     }
 
     @Test
@@ -93,12 +101,14 @@ class MessageUtilsTest{
         val context1: MessageContext = mapOf("extra1" to "value1", "common" to "value1")
         val context2: MessageContext = mapOf("extra2" to "value2", "common" to "value2")
 
-        val result = context1 optAddContext  context2
+        val result = context1 optAddContext context2
 
-        assertThat(result, allOf(
-            hasEntry("extra1", "value1"),
-            hasEntry("extra2", "value2"),
-            hasEntry("common", "value1"),
-        ))
+        assertThat(
+            result, allOf(
+                hasEntry("extra1", "value1"),
+                hasEntry("extra2", "value2"),
+                hasEntry("common", "value1"),
+            )
+        )
     }
 }
