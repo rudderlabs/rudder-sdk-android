be6c36e04f3e30928726dc7b58b34368538bbff8
Chryssa Aliferi <c.aliferi@gmail.com>
2024-07-02 21:44:38 +0300

    feat: minor fix in Properties.kt file

--- core/src/main/java/com/rudderstack/core/Properties.kt
+++ core/src/main/java/com/rudderstack/core/Properties.kt
@@ -1,25 +1,9 @@
-/*
- * Creator: Debanjan Chatterjee on 04/01/22, 11:39 AM Last modified: 04/01/22, 11:39 AM
- * Copyright: All rights reserved Ⓒ 2022 http://rudderstack.com
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License. You may obtain a
- * copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
-
 package com.rudderstack.core
-
-import com.rudderstack.models.*
 
 /**
  * Utility extensions for simplifying APIs
 
-**/
+ **/
 /**
  * Can be used in [Analytics.track] as
  * ```kotlin
@@ -28,7 +12,7 @@ import com.rudderstack.models.*
  *
  * @param value The currency used.
  */
-fun Map<String, Any>.addCurrency(value : String) = this + Pair("currency", value)
+fun Map<String, Any>.addCurrency(value: String) = this + Pair("currency", value)
 
 /**
  * Can be used in [Analytics.track] as
@@ -38,18 +22,17 @@ fun Map<String, Any>.addCurrency(value : String) = this + Pair("currency", value
  *
  * @param value The revenue generated.
  */
-fun Map<String, Any>.addRevenue(value : String) = this + Pair("revenue", value)
+fun Map<String, Any>.addRevenue(value: String) = this + Pair("revenue", value)
 
-fun Map<String, Any>.addCategory(category : String) = this + ("category" to category)
+fun Map<String, Any>.addCategory(category: String) = this + ("category" to category)
 
 /**
  * Used for adding advertisingId to context
  *
  * @param advertisingId The advertising id associated to the application
  */
-fun Map<String, String>.putAdvertisingId(advertisingId : String) = this + ("advertisingId" to advertisingId)
-fun Map<String, String>.putDeviceToken(advertisingId : String) = this + ("advertisingId" to advertisingId)
+fun Map<String, String>.putAdvertisingId(advertisingId: String) = this + ("advertisingId" to advertisingId)
+fun Map<String, String>.putDeviceToken(advertisingId: String) = this + ("advertisingId" to advertisingId)
 
 
-
-fun Map<String, Any?>.with( vararg keyPropertyPair: Pair<String, Any?>) = this + keyPropertyPair
+fun Map<String, Any?>.with(vararg keyPropertyPair: Pair<String, Any?>) = this + keyPropertyPair
