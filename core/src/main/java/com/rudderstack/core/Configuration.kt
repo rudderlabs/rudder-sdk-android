40daf28dededa0c185a5f00b212f8d3726d8eb02
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-07-10 16:49:30 +0530

    chore: remove comments from Configuration class

--- core/src/main/java/com/rudderstack/core/Configuration.kt
+++ core/src/main/java/com/rudderstack/core/Configuration.kt
@@ -9,19 +9,6 @@ import java.util.concurrent.Executors
  * The `Configuration` class defines the settings and parameters used to configure the RudderStack analytics SDK.
  * This class is open for inheritance to allow for customization and extension.
  *
- * @property jsonAdapter A `JsonAdapter` instance used for JSON serialization and deserialization.
- * @property options An instance of `RudderOption` which provides customizable options that can be set globally or on a per-message basis. Defaults to a new `RudderOption` instance.
- * @property flushQueueSize The size of the queue for events to be flushed to the server. Defaults to `FLUSH_QUEUE_SIZE` (30).
- * @property maxFlushInterval The maximum interval (in milliseconds) before events are flushed to the server if the queue size is not reached. Defaults to `MAX_FLUSH_INTERVAL` (10 seconds).
- * @property shouldVerifySdk A flag indicating whether the SDK should be verified. Changing this value post source config download has no effect. Defaults to `false`.
- * @property gzipEnabled A flag indicating whether GZIP compression is enabled for event uploads. Defaults to `true`.
- * @property sdkVerifyRetryStrategy A `RetryStrategy` instance defining the retry strategy for SDK verification. Changing this value post source config download has no effect. Defaults to exponential retry strategy.
- * @property dataPlaneUrl The URL of the data plane where events are sent. Defaults to `DATA_PLANE_URL`.
- * @property controlPlaneUrl The URL of the control plane for fetching the source config. Defaults to `CONTROL_PLANE_URL`.
- * @property logger A `Logger` instance for logging SDK activities. Defaults to an instance of `KotlinLogger`.
- * @property analyticsExecutor An `ExecutorService` for handling analytics tasks. Defaults to a single-thread executor.
- * @property networkExecutor An `ExecutorService` for handling network tasks. Defaults to a cached thread pool executor.
- * @property base64Generator A `Base64Generator` instance for encoding data in base64. Defaults to the `defaultBase64Generator` from `RudderUtils`.
  */
 open class Configuration(
     open val jsonAdapter: JsonAdapter,
