3f1b6da07769dbafeb9e256da685a144b9ccf6b0
Chryssa Aliferi <c.aliferi@gmail.com>
2024-07-17 19:43:10 +0300

    feat: create SharedPrefs file

---
+++ android/src/main/java/com/rudderstack/android/internal/prefs/SharedPrefs.kt
@@ -0,0 +1,16 @@
+package com.rudderstack.android.internal.prefs
+
+object SharedPrefs {
+    internal const val RUDDER_PREFS = "rl_prefs"
+    internal const val RUDDER_TRAITS_KEY = "rl_traits"
+    internal const val RUDDER_TRACK_AUTO_SESSION_KEY = "rl_track_auto_session_key"
+    internal const val RUDDER_EXTERNAL_ID_KEY = "rl_external_id"
+    internal const val RUDDER_OPT_STATUS_KEY = "rl_opt_status"
+    internal const val RUDDER_ANONYMOUS_ID_KEY = "rl_anonymous_id_key"
+    internal const val RUDDER_USER_ID_KEY = "rl_user_id_key"
+    internal const val RUDDER_SESSION_ID_KEY = "rl_session_id_key"
+    internal const val RUDDER_SESSION_LAST_ACTIVE_TIMESTAMP_KEY = "rl_last_event_timestamp_key"
+    internal const val RUDDER_ADVERTISING_ID_KEY = "rl_advertising_id_key"
+    internal const val RUDDER_APPLICATION_VERSION_KEY = "rl_application_version_key"
+    internal const val RUDDER_APPLICATION_BUILD_KEY = "rl_application_build_key"
+}
