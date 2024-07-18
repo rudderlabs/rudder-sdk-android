14a17813af1131e1a2639ee1a06297eb19e75b0d
Chryssa Aliferi <c.aliferi@gmail.com>
2024-07-18 18:56:23 +0300

    feat: applies changes to StoragePlugin

--- core/src/main/java/com/rudderstack/core/internal/plugins/StoragePlugin.kt
+++ core/src/main/java/com/rudderstack/core/internal/plugins/StoragePlugin.kt
@@ -3,7 +3,6 @@ package com.rudderstack.core.internal.plugins
 import com.rudderstack.core.Analytics
 import com.rudderstack.core.Plugin
 import com.rudderstack.core.models.Message
-import java.lang.ref.WeakReference
 
 /**
  * Adds [Message] to repository for further processing.
@@ -12,19 +11,13 @@ import java.lang.ref.WeakReference
  *
  */
 internal class StoragePlugin : Plugin {
-    private var _analytics: WeakReference<Analytics?> = WeakReference(null)
-    override fun setup(analytics: Analytics) {
-        _analytics = WeakReference(analytics)
-    }
+
+    override lateinit var analytics: Analytics
 
     override fun intercept(chain: Plugin.Chain): Message {
         val message = chain.message()
-        _analytics.get()?.storage?.saveMessage(message.copy())
-
+        analytics.storage.saveMessage(message.copy())
         return chain.proceed(message)
     }
 
-    override fun onShutDown() {
-        _analytics = WeakReference(null)
-    }
 }
